a60afb3288a4d3e5798547e2bbc9f03e8e1f8cba
GeorgeGeorgiou <ggeorgiou76@hotmail.com>
2022-10-28 18:49:30 +0300

    Removemanagementgroups

--- main.tf
+++ main.tf
@@ -76,22 +76,3 @@ resource "azurerm_nat_gateway" "myrg" {
 
 }
 
-data "azurerm_subscription" "current" {
-}
-resource "azurerm_management_group" "example_parent" {
-  display_name = "ParentGroup"
-
-  subscription_ids = [
-    data.azurerm_subscription.current.subscription_id,
-  ]
-}
-
-resource "azurerm_management_group" "example_child" {
-  display_name               = "ChildGroup"
-  parent_management_group_id = azurerm_management_group.example_parent.id
-
-  subscription_ids = [
-    data.azurerm_subscription.current.subscription_id,
-  ]
-  # other subscription IDs can go here
-}

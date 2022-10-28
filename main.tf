3910de96cb26bc8e09e334a057d5dfe394947be7
GeorgeGeorgiou <ggeorgiou76@hotmail.com>
2022-10-28 11:52:29 +0300

    save13

--- main.tf
+++ main.tf
@@ -53,4 +53,11 @@ provider "azurerm" {
   }
   
 
- 
+  resource "azure_public_ip" "myrgpublic" {
+  name = "nat-gateway-public"
+  location = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+    allocation_method   = "Static"
+  sku = "Standard"
+  zones = ["1"]
+  }
